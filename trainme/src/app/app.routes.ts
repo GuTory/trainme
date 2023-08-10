ef87e3da113b2337352ccb787798716183a58f46
Kristof Toth <zmgtothkr@gmail.com>
2023-08-10 21:21:21 +0200

    basic routes

--- trainme/src/app/app.routes.ts
+++ trainme/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './shared/components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent,
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes').then(m => m.authRoutes),
+    },
+];
